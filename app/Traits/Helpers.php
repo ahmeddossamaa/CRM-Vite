5e6d199efdfe5a612b9cb2daffc1f394501bb247
ahmeddossamaa <ahmeddossamaa184@gmail.com>
2022-11-21 09:45:31 +0200

    updates from ubuntu

--- app/Traits/Helpers.php
+++ app/Traits/Helpers.php
@@ -6,16 +6,8 @@ trait Helpers
 {
     public function getResponse($status, $message = '', $data = '', $code = 200): \Illuminate\Http\JsonResponse
     {
-        if (empty($code)) {
-            switch ($status) {
-                case 'success':
-                    $code = 200;
-                    break;
-                case 'error':
-                    $code = 422;
-                    break;
-            }
-        }
+        $code = !empty($code)? $code: ($status == 'success'? 200: 422);
+
         return response()->json([
             'message' => $message,
             'data' => $data,
